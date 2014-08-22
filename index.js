bb26ebbbef5600ad4f5beebe9f9cb3a6aa3aea03
Maxime Thirouin <m@moox.io>
2014-08-22 06:06:24 +0200

    expose less stuff (this was stupid)

--- index.js
+++ index.js
@@ -7,49 +7,26 @@ var transformHwb = require("./lib/transform-hwb")
 var transformRebeccapurple = require("./lib/transform-rebeccapurple")
 
 /**
- * Expose plugin, normalizeOptions & transformDecl
- */
-module.exports = plugin
-module.exports.normalizeOptions = normalizeOptions
-module.exports.transformDecl = transformDecl
-
-/**
  * PostCSS plugin to convert colors
  *
  * @param {Object} options
  */
-function plugin(options) {
-  normalizeOptions(options)
-
-  return function(style) {
-    style.eachDecl()
-  }
-}
-
-/**
- * normalize options
- *
- * @param {Object} options
- */
-function normalizeOptions(options) {
+module.exports = function plugin(options) {
   options = options || {}
   options.rebeccapurple = options.rebeccapurple !== undefined ? options.rebeccapurple : true
   options.hwb = options.hwb !== undefined ? options.hwb : true
   options.hexAlpha = options.hexAlpha !== undefined ? options.hexAlpha : true
   options.color = options.color !== undefined ? options.color : true
-}
 
-/**
- * Transform colors to rgb() or rgba()  on a declaration object
- *
- * @param {Object} dec
- */
-function transformDecl(dec) {
-  if (!dec.value) {
-    return
+  return function(style) {
+    style.eachDecl(function transformDecl(dec) {
+      if (!dec.value) {
+        return
+      }
+
+      dec.value = transform(dec.value, dec.source, options)
+    })
   }
-
-  dec.value = transform(dec.value, dec.source, options)
 }
 
 /**
