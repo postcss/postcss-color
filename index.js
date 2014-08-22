62964ede010e209429e02ed22f0bf24df1fdc167
Maxime Thirouin <m@moox.io>
2014-08-22 06:02:52 +0200

    Expose more stuff

--- index.js
+++ index.js
@@ -7,36 +7,57 @@ var transformHwb = require("./lib/transform-hwb")
 var transformRebeccapurple = require("./lib/transform-rebeccapurple")
 
 /**
+ * Expose plugin, normalizeOptions & transformDecl
+ */
+module.exports = plugin
+module.exports.normalizeOptions = normalizeOptions
+module.exports.transformDecl = transformDecl
+
+/**
  * PostCSS plugin to convert colors
  *
  * @param {Object} options
  */
+function plugin(options) {
+  normalizeOptions(options)
 
-module.exports = function plugin(options) {
+  return function(style) {
+    style.eachDecl()
+  }
+}
+
+/**
+ * normalize options
+ *
+ * @param {Object} options
+ */
+function normalizeOptions(options) {
   options = options || {}
   options.rebeccapurple = options.rebeccapurple !== undefined ? options.rebeccapurple : true
   options.hwb = options.hwb !== undefined ? options.hwb : true
   options.hexAlpha = options.hexAlpha !== undefined ? options.hexAlpha : true
   options.color = options.color !== undefined ? options.color : true
-
-  return function(style) {
-    style.eachDecl(function declaration(dec) {
-      if (!dec.value) {
-        return
-      }
-
-      dec.value = transform(dec.value, dec.source, options)
-    })
-  }
 }
 
 /**
- * Transform colors to rgb() or rgba()
+ * Transform colors to rgb() or rgba()  on a declaration object
+ *
+ * @param {Object} dec
+ */
+function transformDecl(dec) {
+  if (!dec.value) {
+    return
+  }
+
+  dec.value = transform(dec.value, dec.source, options)
+}
+
+/**
+ * Transform colors to rgb() or rgba() on a declaration value
  *
  * @param {String} string
  * @return {String}
  */
-
 function transform(string, source, options) {
   // order of transformation is important
 
